9a12fadc3e0f3e5714d2de55c6987f00476db392
Darya Kauzova <amayasuzuki12@gmail.com>
2025-07-19 21:18:11 +0300

    fixed a little bug

--- src/components/pages/cart/CartUnauthorized.js
+++ src/components/pages/cart/CartUnauthorized.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const CartUnauthorized = () => {
   return (
     <div className='cart__unauthorized'>
-        <h3 className='cart__unauthorized_title'>Для доступа к корзине требуется вйоти в аккаунт или зарегистрироваться</h3>
+      <h3 className='cart__unauthorized_title'>Для доступа к корзине требуется войти в аккаунт или зарегистрироваться</h3>
     </div>
   )
 }
